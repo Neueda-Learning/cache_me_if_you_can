5564a86435c9e973d23ecd422030b1c617bfaab0
Sevengods77 <vhg772004@gmail.com>
2026-07-30 12:32:59 +0530

    Initial SpringBoot Setup

---
+++ src/test/java/com/neueda/transaction_monitor/TransactionMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.transaction_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
